db9adc5491c17ba1d42e73f93ce29def1e274333
Rebecca Long <rebecca.long@gmail.com>
2016-02-20 11:16:51 -0800

    Fixed merge conflicts

---
+++ Hackathon/Hackathon.Repository/DataAccess/BoardItemRepository.cs
@@ -0,0 +1,22 @@
+﻿namespace Hackathon.Repository.DataAccess
+{
+    using Core.Models;
+    using System.Data.SQLite;
+
+    public interface IBoardItemRepository
+    {
+        void SaveBoardItems(BoardItem[] itemsToSave);
+    }
+
+    public class BoardItemRepository
+    {
+
+        public void SaveBoardItems(BoardItem[] itemsToSave)
+        {
+            using (var connection = new SQLiteConnection())
+            {
+
+            }
+        }
+    }
+}
