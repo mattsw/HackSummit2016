b6f54fa92c57f58981643829d35c10bf0c6e76a6
Rebecca Long <rebecca.long@gmail.com>
2016-02-20 10:31:45 -0800

    Added base project structure and added a few dlls and a start model

---
+++ Hackathon/Hackathon.Core/Models/BoardItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Hackathon.Core.Models
+{
+    public class BoardItem
+    {
+        public int TaskID { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Hackathon/Hackathon.Web/ApiControllers/TaskController.cs
@@ -0,0 +1,19 @@
+﻿namespace Hackathon.Web.ApiControllers
+{
+    using System.Web.Http;
+    using System.Net;
+    using Core.Models;
+     
+    public class TaskController : ApiController
+    {
+        public IHttpActionResult Get(string id)
+        {
+            return StatusCode(HttpStatusCode.NotImplemented);
+        }
+
+        public IHttpActionResult Post(BoardItem[] boardItems)
+        {
+            return StatusCode(HttpStatusCode.NotImplemented);
+        }
+    }
+}
