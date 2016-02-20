696c36106e664b53e5001433b8e9157f91f76993
Rebecca Long <mattwilhelm@cet.com>
2016-02-20 15:18:30 -0800

    added camel case thing that matt loves

--- Hackathon/Hackathon.Web/App_Start/WebApiConfig.cs
+++ Hackathon/Hackathon.Web/App_Start/WebApiConfig.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Newtonsoft.Json.Serialization;
 using System.Web.Http;
 
 namespace Hackathon.Web
@@ -13,6 +11,7 @@ namespace Hackathon.Web
 
             // Web API routes
             config.MapHttpAttributeRoutes();
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
