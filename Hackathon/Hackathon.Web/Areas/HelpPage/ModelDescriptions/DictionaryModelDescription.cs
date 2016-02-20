c9db0c263c09a043167be3f4a750786b2303e1ba
Kelly Rankin <kellyprankin@gmail.com>
2016-02-20 09:22:48 -0800

    Initial commit

---
+++ Hackathon/Hackathon.Web/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Hackathon.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Hackathon/Hackathon.Web/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Hackathon.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
