b3e5fbb312f2ad7d55d0c3caed204a8bb95d3a0a
Kelly Rankin <kellyprankin@gmail.com>
2016-02-20 11:05:45 -0800

    Removing bad files

--- Hackathon/Hackathon.Web/App_Start/BundleConfig.cs
+++ Hackathon/Hackathon.Web/App_Start/BundleConfig.cs
@@ -11,11 +11,6 @@ namespace Hackathon.Web
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
@@ -23,6 +18,14 @@ namespace Hackathon.Web
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/js/lib").Include(
+                "~/Scripts/angular.js",
+                "~/Scripts/angular-ui-router.js"
+                ));
+
+            bundles.Add(new ScriptBundle("~/js/app").IncludeDirectory(
+                "~/client/", "*.js", true));
         }
     }
 }
---
+++ Hackathon/Hackathon.Web/client/app.js
@@ -0,0 +1,32 @@
+﻿(function (angular) {
+    angular
+        .module('board', ['ui.router'])
+        .config(config)
+        .run(run);
+
+    config.$inject = ['$stateProvider', '$locationProvider', '$urlRouterProvider'];
+
+    function config($stateProvider, $locationProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('main', {
+                templateUrl: 'client/main/main.html',
+                controller: 'MainCtrl as vm'
+            });
+    }
+
+    run.$inject = ['$state', '$rootScope'];
+
+    function run($state, $rootScope) {
+        $state.go('main');
+
+        $rootScope.$on('stateChangeStart',
+            function (event, toState, toParams, fromState, fromParams, options) {
+            });
+
+        $rootScope.$on('stateChangeError',
+            function (event, toState, toParams, fromState, fromParams, error) {
+
+        });
+    }
+
+})(window.angular);
