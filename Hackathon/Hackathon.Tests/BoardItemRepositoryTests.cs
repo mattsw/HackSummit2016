2499e3241ce815144ad3833598f2f7dc8b3d090c
Rebecca Long <rebecca.long@gmail.com>
2016-02-20 18:17:17 -0800

    Adding tests

---
+++ Hackathon/Hackathon.Tests/BoardItemRepositoryTests.cs
@@ -0,0 +1,21 @@
+﻿namespace Hackathon.Tests
+{
+    using Core.Models.Board;
+    using NUnit.Framework;
+    using Repository.DataAccess;
+
+    [TestFixture]
+    public class BoardItemRepositoryTests
+    {
+        [TestFixture]
+        public class CreateBoardItemsTests
+        {
+            [TestCase]
+            public void Create_SingleOpenItem()
+            {
+                var sut = new BoardItemRepository();
+                var boardItem = new BoardItem() {  };
+            }
+        }
+    }
+}
